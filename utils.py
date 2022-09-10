f771758994223ada067c61f7b7659154078c2872
faizan12345 <mohammadfaizantanoli@gmail.com>
2022-09-11 01:26:15 +0500

    xyz

--- utils.py
+++ utils.py
@@ -165,7 +165,7 @@ class util:
         return image, label  
           
     @staticmethod
-    def separte_label(case):
+    def separate_label(case):
         '''separate an MRI image to it's label
         case: patient id with mri modaliteis (path)
         return
@@ -184,32 +184,33 @@ class util:
                     img = (data - data_min) / (np.max(data) - data_min)
                     image.append(img)
                 else:
-                     mask_path =  os.path.join(case, modality)
-                     mask = nib.load(mask_path)
-                     mask = np.asanyarray(mask.dataobj)
-                     mask_WT = mask.copy()
-                     mask_WT[mask_WT == 1] = 1
-                     mask_WT[mask_WT == 2] = 1
-                     mask_WT[mask_WT == 4] = 1
 
-                     mask_TC = mask.copy()
-                     mask_TC[mask_TC == 1] = 1
-                     mask_TC[mask_TC == 2] = 0
-                     mask_TC[mask_TC == 4] = 1
+                    mask_path =  os.path.join(case, modality)
+                    mask = nib.load(mask_path)
+                    mask = np.asanyarray(mask.dataobj)
+                    mask_WT = mask.copy()
+                    mask_WT[mask_WT == 1] = 1
+                    mask_WT[mask_WT == 2] = 1
+                    mask_WT[mask_WT == 4] = 1
 
-                     mask_ET = mask.copy()
-                     mask_ET[mask_ET == 1] = 0
-                     mask_ET[mask_ET == 2] = 0
-                     mask_ET[mask_ET == 4] = 1
+                    mask_TC = mask.copy()
+                    mask_TC[mask_TC == 1] = 1
+                    mask_TC[mask_TC == 2] = 0
+                    mask_TC[mask_TC == 4] = 1
+
+                    mask_ET = mask.copy()
+                    mask_ET[mask_ET == 1] = 0
+                    mask_ET[mask_ET == 2] = 0
+                    mask_ET[mask_ET == 4] = 1
                      
-                     mask = np.stack([mask_WT, mask_TC, mask_ET])
-                     mask = np.moveaxis(mask, (0, 1, 2, 3), (0, 3, 2, 1))
-                     label.append(mask)
+                    mask = np.stack([mask_WT, mask_TC, mask_ET])
+                    mask = np.moveaxis(mask, (0, 1, 2, 3), (0, 3, 2, 1))
+                    label.append(mask)
 
             global image_data       
             image_data = np.stack(image)
             image_data = np.moveaxis(image_data, (0, 1, 2, 3), (0, 3, 2, 1))
-        return (image_data, label[0])
+            return (image_data, label[0])
 
                     
 
