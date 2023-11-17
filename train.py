6ee9d6bc2b827d71fcea1c62999e1ca2e24c2b1c
unknown <mohammadfaizantanoli@gmail.com>
2023-11-17 15:26:49 +0500

    updated the training script

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 """
 train BraTS21 Tumor segmentation model
+--------------------------------------
+
+
 Author: Muhammad Faizan
 Date: 5 May 2023
 National University of Sciences and Technology Islamabad, Pakistan
