63d301ea89f7b961665bac6e2df6a746a53813ca
MFaizan <mohammadfaizantanoli@gmail.com>
2024-10-14 18:04:51 +0500

    updated model

--- test_model.py
+++ test_model.py
@@ -1,9 +1,14 @@
 import torch
 from research.models.SegConvNeXt.segconvnext import SegConvNeXt
+from research.models.SegConvNeXt.segconvnextv1 import SegConvNeXtv1
+from monai.networks.nets import SegResNet
 
 
-input = torch.rand(1, 4, 160, 192, 128).cuda()
-model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
+
+input = torch.rand(1, 4, 160, 192, 128).cpu()
+# model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
+model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
+                  ).cpu()
 
 # output
 out = model(input)
