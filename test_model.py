3afe8692ab1a47d7f1489220d7d2de68a95f4bb5
faizan1234567 <mohammadfaizantanoli@gmail.com>
2024-10-14 16:14:47 +0500

    updated the test code

--- test_model.py
+++ test_model.py
@@ -5,5 +5,6 @@ from research.models.SegConvNeXt.segconvnext import SegConvNeXt
 input = torch.rand(1, 4, 160, 192, 128).cuda()
 model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
 
+# output
 out = model(input)
 print(out.shape)
