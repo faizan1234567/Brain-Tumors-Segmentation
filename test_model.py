61ffb3fd1b1488da68ba6bae64ce6573b0056501
MFaizan <mohammadfaizantanoli@gmail.com>
2024-10-27 18:14:14 +0500

    SegUXNet added

--- test_model.py
+++ test_model.py
@@ -1,27 +1,21 @@
 import torch
-from research.models.SegConvNeXt.segconvnext import SegConvNeXt
-from research.models.SegConvNeXt.segconvnextv1 import SegConvNeXtv1
-from monai.networks.nets import SegResNet
-from monai.networks.nets import SwinUNETR
-
+import torch.nn as nn
+from monai.networks.blocks.segresnet_block import ResBlock
+from research.models.UX_Net.uxnet_encoder import ux_block
+from research.models.SegUXNet.model import SegUXNet
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 input = torch.rand(1, 4, 128, 128, 128).to(device)
 # model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
 # model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
-#                   ).cpu()\
-# SWIN UNETR 
-model = model = SwinUNETR(
-                img_size=128,
-                in_channels=4,
-                out_channels=3,
-                feature_size=48,
-                drop_rate=0.1,
-                attn_drop_rate=0.2,
-                dropout_path_rate=0.1,
-                spatial_dims=3,
-                use_checkpoint=False,
-                use_v2=False).to(device)
-# output
+#                   ).cpu()
+
+
+# ux_blk = ux_block(dim=4, drop_path=0.1)
+
+# res_blk = ResBlock(spatial_dims=3, in_channels=4, kernel_size=3)
+model = SegUXNet(spatial_dims=3, init_filters=32, 
+                 in_channels=4, out_channels=3, dropout_prob=0.2, 
+                 )
 out = model(input)
 print(out.shape)
