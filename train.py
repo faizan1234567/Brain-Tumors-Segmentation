966a333e244e7727f755d6a37b70dc414b549fb1
MFaizan <mohammadfaizantanoli@gmail.com>
2024-10-27 19:01:59 +0500

    training code updated

--- train.py
+++ train.py
@@ -525,7 +525,8 @@ def main(cfg: DictConfig):
                          out_channels=num_classes, 
                          dropout_prob=0.2, 
                          blocks_down=(1, 2, 2, 4), 
-                         blocks_up=(1, 1, 1)).to(device),
+                         blocks_up=(1, 1, 1)).to(device)
+        model = model[0]
         
     print('Chosen Network Architecture: {}'.format(cfg.model.architecture))
     roi = cfg.model.roi
