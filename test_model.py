cb78f2022097284520aae4e82a98983d3245209f
faizan1234567 <mohammadfaizantanoli@gmail.com>
2024-10-29 09:40:07 +0500

    updated the test model code

--- test_model.py
+++ test_model.py
@@ -4,6 +4,7 @@ from monai.networks.blocks.segresnet_block import ResBlock
 from research.models.UX_Net.uxnet_encoder import ux_block
 from research.models.SegUXNet.model import SegUXNet
 
+# select device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 input = torch.rand(1, 4, 128, 128, 128).to(device)
 # model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
