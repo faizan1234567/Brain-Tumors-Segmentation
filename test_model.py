2df19b3fb6a6a84ccc4e363defea5bc1ff8fd6e1
MFaizan <mohammadfaizantanoli@gmail.com>
2024-10-31 18:37:01 +0500

    updated the test code

--- test_model.py
+++ test_model.py
@@ -9,8 +9,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 input = torch.rand(1, 4, 128, 128, 128).to(device)
 # model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
 # model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
-#                   )
-
+# ).cpu()
 model = SegUXNet(spatial_dims=3, init_filters=32, 
                  in_channels=4, out_channels=3, dropout_prob=0.2, 
                  )
