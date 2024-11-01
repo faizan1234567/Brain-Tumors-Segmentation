f02c38b6e20a65eb6da44df48897b9d43494494a
faizan1234567 <mohammadfaizantanoli@gmail.com>
2024-11-01 10:04:21 +0500

    commenting test code

--- test_model.py
+++ test_model.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
 from monai.networks.blocks.segresnet_block import ResBlock
-from research.models.UX_Net.uxnet_encoder import ux_block
-from research.models.SegUXNet.model import SegUXNet
+# from research.models.UX_Net.uxnet_encoder import ux_block
+# from research.models.SegUXNet.model import SegUXNet
 
-# select device
-device = "cuda" if torch.cuda.is_available() else "cpu"
-input = torch.rand(1, 4, 128, 128, 128).to(device)
-# model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
-# model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
-# ).cpu()
-model = SegUXNet(spatial_dims=3, init_filters=32, 
-                 in_channels=4, out_channels=3, dropout_prob=0.2, 
+# # select device
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# input = torch.rand(1, 4, 128, 128, 128).to(device)
+# # model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
+# # model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
+# # ).cpu()
+# model = SegUXNet(spatial_dims=3, init_filters=32, 
+#                  in_channels=4, out_channels=3, dropout_prob=0.2, 
                  )
-out = model(input)
-print(out.shape)
+# out = model(input)
+# print(out.shape)
