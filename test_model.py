76ee44f9a6ed07f76a2154f39708f9859fc28796
MFaizan <mohammadfaizantanoli@gmail.com>
2024-11-03 15:13:26 +0500

    updated the test_model code

--- test_model.py
+++ test_model.py
@@ -2,16 +2,21 @@ import torch
 import torch.nn as nn
 from monai.networks.blocks.segresnet_block import ResBlock
 # from research.models.UX_Net.uxnet_encoder import ux_block
-# from research.models.SegUXNet.model import SegUXNet
+from thesis.models.SegUXNet.model import SegUXNet
 
 # # select device
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# input = torch.rand(1, 4, 128, 128, 128).to(device)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+input = torch.rand(1, 4, 128, 128, 128).to(device)
 # # model = SegConvNeXt(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3).cuda()
 # # model = SegConvNeXtv1(spatial_dims=3, init_filters=32, in_channels=4, out_channels=3
 # # )
-# model = SegUXNet(spatial_dims=3, init_filters=32, 
-#                  in_channels=4, out_channels=3, dropout_prob=0.2, 
-                 
-# out = model(input)
-# print(out.shape)
+model = SegUXNet(spatial_dims=3, 
+                init_filters=32, 
+                in_channels= 4,
+                out_channels=3, 
+                dropout_prob=0.2, 
+                blocks_down=(1, 2, 2, 4), 
+                blocks_up=(1, 1, 1), 
+                enable_gc=True).to(device)
+out = model(input)
+print(out.shape)
